8f96a76a5eeefff0244f19286d3b2f236fb2d4db
Stephen Cox <stephen-cox@users.noreply.github.com>
2020-07-22 09:36:20 +0100

    Change title and lede on guide pages (#21)
    * Added event subscriber to alter the page header for guide pages. localgovdrupal/localgov/issues/87

---
+++ src/EventSubscriber/PageHeaderSubscriber.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\localgov_guides\EventSubscriber;
+
+use Drupal\node\Entity\Node;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\localgov_core\Event\PageHeaderDisplayEvent;
+
+/**
+ * Class PageHeaderSubscriber.
+ *
+ * @package Drupal\localgov_guides\EventSubscriber
+ */
+class PageHeaderSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      PageHeaderDisplayEvent::EVENT_NAME => ['setPageHeader', 0],
+    ];
+  }
+
+  /**
+   * Set page title and lede.
+   */
+  public function setPageHeader(PageHeaderDisplayEvent $event) {
+    if ($event->getEntity() instanceof Node &&
+      $event->getEntity()->bundle() == 'localgov_guides_page' &&
+      $event->getEntity()->localgov_guides_parent
+    ) {
+      $overview = $event->getEntity()->localgov_guides_parent->entity;
+      $event->setTitle($overview->getTitle());
+      if ($overview->get('body')->summary) {
+        $event->setLede([
+          '#type' => 'html_tag',
+          '#tag' => 'p',
+          '#value' => $overview->get('body')->summary,
+        ]);
+      }
+      else {
+        $event->setLede('');
+      }
+    }
+  }
+
+}
