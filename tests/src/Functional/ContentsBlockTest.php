bf28ef5838fa901fcec38e97491e86530dda9de5
ekes <ekes@iskra.net>
2020-10-28 16:50:56 +0100

    Add tests currently failing to be fixed: Unpublished and moved content.

--- tests/src/Functional/ContentsBlockTest.php
+++ tests/src/Functional/ContentsBlockTest.php
@@ -142,7 +142,61 @@ class ContentsBlockTest extends BrowserTestBase {
       'localgov_guides_parent' => ['target_id' => $overview->id()],
     ]);
     $this->drupalGet($overview->toUrl()->toString());
+    $xpath = '//ul[@class="progress"]/li';
+    $results = $this->xpath($xpath);
+    $this->assertEquals(5, count($results));
     $this->assertText('Guide page 4');
+    // Change title.
+    $pages[2]->title = 'New title page 2';
+    $pages[2]->save();
+    $this->drupalGet($overview->toUrl()->toString());
+    $this->assertNoText('Guide page 2');
+    $this->assertText('New title page 2');
+
+    // Another overview.
+    $overview_2 = $this->createNode([
+      'title' => 'Guide overview 2',
+      'type' => 'localgov_guides_overview',
+      'status' => NodeInterface::PUBLISHED,
+    ]);
+    // Move a current revision to it.
+    $pages[0]->localgov_guides_parent = ['target_id' => $overview_2->id()];
+    $pages[0]->setNewRevision();
+    $pages[0]->save();
+    // Check overview.
+    $this->drupalGet($overview->toUrl()->toString());
+    $xpath = '//ul[@class="progress"]/li';
+    $results = $this->xpath($xpath);
+    $this->assertEquals(4, count($results));
+    $this->assertNotContains('Guide page 0', $results[1]->getText());
+    // Check new overview.
+    $this->drupalGet($overview_2->toUrl()->toString());
+    $xpath = '//ul[@class="progress"]/li';
+    $results = $this->xpath($xpath);
+    $this->assertEquals(2, count($results));
+    $this->assertContains('Guide overview', $results[0]->getText());
+    $this->assertNotContains($overview_2->toUrl()->toString(), $results[0]->getHtml());
+    $this->assertContains('Guide page 0', $results[1]->getText());
+    $this->assertContains($pages[0]->toUrl()->toString(), $results[1]->getHtml());
+
+    // Unpublish a page.
+    $pages[1]->status = NodeInterface::NOT_PUBLISHED;
+    $pages[1]->save();
+    // Still linked.
+    $content_admin = $this->drupalCreateUser(['bypass node access']);
+    $this->drupalLogin($content_admin);
+    $this->drupalGet($overview->toUrl()->toString());
+    $xpath = '//ul[@class="progress"]/li';
+    $results = $this->xpath($xpath);
+    $this->assertEquals(4, count($results));
+    $this->assertText('Guide page 1');
+    $this->drupalLogout();
+    // But not for anon.
+    $this->drupalGet($overview->toUrl()->toString());
+    $xpath = '//ul[@class="progress"]/li';
+    $results = $this->xpath($xpath);
+    $this->assertEquals(3, count($results));
+    $this->assertNoText('Guide page 1');
   }
 
 }
